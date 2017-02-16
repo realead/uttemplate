6402d0d08398cf674db6ce6488ef995508a0fbf6
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2017-02-16 22:10:42 +0100

    making it possible to use not only types but also other types of parameters

--- uttemplate/uttemplate.py
+++ uttemplate/uttemplate.py
@@ -20,17 +20,24 @@ __TEST_CASE_MIDDLE="_for_"
 
 def mangle_name(name):
     return __TEST_CASE_PREFIX+name+__TEST_CASE_MIDDLE
+    
+#actually my_type don't have to be a type..    
+def get_type_name(my_type):
+    if hasattr(my_type, "__name__"):
+        return my_type.__name__
+    else:
+        return str(my_type)
 
 def tests_from_free_function(fun, target_cls, types):
     for my_type in types:
-        method_name=mangle_name(fun.__name__)+my_type.__name__
+        method_name=mangle_name(fun.__name__)+get_type_name(my_type)
         method_name=find_unused_name(method_name, target_cls.__dict__) 
         setattr(target_cls, method_name, lambda self, inner_type=my_type: fun(inner_type) if fun.__code__.co_argcount==1 else fun(inner_type, self))#x=self
 
 
 def tests_from_member(fun, target_cls, types):
     for my_type in types:
-        method_name=mangle_name(fun.__name__)+my_type.__name__
+        method_name=mangle_name(fun.__name__)+get_type_name(my_type)
         method_name=find_unused_name(method_name, target_cls.__dict__) 
         setattr(target_cls, method_name, lambda x, inner_type=my_type: fun(x, inner_type))#x=self
   
