e13b68251570f3bb59e23fff4deef4f971af738f
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2017-12-26 22:35:10 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='uttemplate',
+      version='0.1.0',
+      description='Extension for unittest module, adding test templates',
+      author='Egor Dranischnikow',
+      url='https://github.com/realead/uttemplate',
+      packages=['uttemplate'],
+     )
