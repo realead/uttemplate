05ae26b3ffc0e39ee896a09c5c37a2c7e54e592e
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2018-06-23 22:59:26 +0200

    incrementing version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='uttemplate',
-      version='0.1.0',
+      version='0.1.1',
       description='Extension for unittest module, adding test templates',
       author='Egor Dranischnikow',
       url='https://github.com/realead/uttemplate',
--- uttemplate/__init__.py
+++ uttemplate/__init__.py
@@ -1,2 +1,5 @@
+__version__=(0,1,1)
+
 
 from .uttemplate import from_nonmember, from_templates, for_types
+
